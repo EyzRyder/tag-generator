b9818d2b1c32f83acccbca940799f01f03928561
Gabriel Bessi <85580011+EyzRyder@users.noreply.github.com>
2024-02-08 21:23:07 -0300

    feat: implemented fomat in controller

--- python/src/views/tag_creator_view.py
+++ python/src/views/tag_creator_view.py
@@ -1,3 +1,4 @@
+from src.controllers.tag_creator_controller import TagCreatorControler
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 
@@ -9,9 +10,11 @@ class TagCreaterView:
     '''
 
     def validate_and_creat(self,http_request:HttpRequest) -> HttpResponse:
-        body: http_request.body
-        pruduct_code = body["product_code"]
+        tag_creator_controller = TagCreatorControler()
 
-        print('View')
+        body = http_request.body
+        product_code = body["product_code"]
 
-        return HttpResponse(status_code=200,body={"Name":"DOe"})
+        formatted_response = tag_creator_controller.create(product_code)
+
+        return HttpResponse(status_code=200,body=formatted_response)
